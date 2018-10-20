b18c5ba41c93fcf1ada201e669c881ddd01105e6
Amberley Romo <amberleyjohanna@gmail.com>
2018-10-20 14:59:36 -0500

    default to async await in gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,62 +1,56 @@
 exports.createPages = async ({ graphql, actions: { createPage } }) => {
-  return new Promise((resolve, reject) => {
-    graphql(
-      `
-        query {
-          allPokeapiPokemon {
-            edges {
-              node {
-                name
-                id
-                abilities {
-                  id
-                  name
-                }
-              }
+  const result = await graphql(`
+    query {
+      allPokeapiPokemon {
+        edges {
+          node {
+            name
+            id
+            abilities {
+              id
+              name
             }
           }
         }
-      `
-    ).then(result => {
-      if (result.errors) {
-        return reject(result.errors);
       }
+    }
+  `);
 
-      // Create a page that lists all Pokémon.
+  const {
+    data: {
+      allPokeapiPokemon: { edges: allPokemon }
+    }
+  } = result;
+
+  // Create a page that lists all Pokémon.
+  createPage({
+    path: `/`,
+    component: require.resolve("./src/templates/all-pokemon.js"),
+    context: {
+      slug: `/`
+    }
+  });
+
+  // Create a page for each Pokémon.
+  allPokemon.forEach(pokemon => {
+    createPage({
+      path: `/pokemon/${pokemon.node.name}/`,
+      component: require.resolve("./src/templates/pokemon.js"),
+      context: {
+        name: pokemon.node.name
+      }
+    });
+
+    // Create a page for each ability of the current Pokémon.
+    pokemon.node.abilities.forEach(ability => {
       createPage({
-        path: `/`,
-        component: require.resolve("./src/templates/all-pokemon.js"),
+        path: `/pokemon/${pokemon.node.name}/ability/${ability.name}/`,
+        component: require.resolve("./src/templates/ability.js"),
         context: {
-          slug: `/`
+          pokemonId: pokemon.node.id,
+          abilityId: ability.id
         }
       });
-
-      const allPokemon = result.data.allPokeapiPokemon.edges;
-
-      // Create a page for each Pokémon.
-      allPokemon.forEach(pokemon => {
-        createPage({
-          path: `/pokemon/${pokemon.node.name}/`,
-          component: require.resolve("./src/templates/pokemon.js"),
-          context: {
-            name: pokemon.node.name
-          }
-        });
-
-        // Create a page for each ability of the current Pokémon.
-        pokemon.node.abilities.forEach(ability => {
-          createPage({
-            path: `/pokemon/${pokemon.node.name}/ability/${ability.name}/`,
-            component: require.resolve("./src/templates/ability.js"),
-            context: {
-              pokemonId: pokemon.node.id,
-              abilityId: ability.id
-            }
-          });
-        });
-      });
-
-      return resolve();
     });
   });
 };
